809d2556da982017a23a1daa666b2fd4f717db11
Silviu Bogan <silviubogan@gmail.com>
2020-05-14 15:47:39 +0300

    First attempt

--- src/editor/components/Button.jsx
+++ src/editor/components/Button.jsx
@@ -1,26 +1,33 @@
 import cx from 'classnames';
 import React from 'react';
 import { Icon } from '@plone/volto/components';
-
-// import { Button as UIButton } from 'semantic-ui-react';
+import { Button as UIButton } from 'semantic-ui-react';
 
 const Button = React.forwardRef(
   ({ className, active, reversed, icon, style, ...props }, ref) => {
     style = {
       ...style,
+      display: 'inline-block',
       cursor: 'pointer',
-      color: reversed
-        ? active
-          ? 'white'
-          : '#888'
-        : active
-        ? ' black'
-        : '#888',
+      // color: reversed
+      //   ? active
+      //     ? 'white'
+      //     : '#888'
+      //   : active
+      //   ? ' black'
+      //   : '#888',
     };
     return (
-      <span {...props} ref={ref} style={style} className={cx(className)}>
+      <UIButton
+        {...props}
+        active={active}
+        ref={ref}
+        style={style}
+        className={cx(className)}
+        size="mini"
+      >
         {icon ? <Icon name={icon} size="24px" /> : 'no-icon'}
-      </span>
+      </UIButton>
     );
   },
 );
