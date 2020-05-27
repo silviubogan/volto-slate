896908c53b1197c1db09e0da059054bb6788e20d
Silviu Bogan <silviubogan@gmail.com>
2020-05-27 17:56:11 +0300

    Remove the ObjectWidget as it will be integrated in Volto

--- src/futurevolto/config.js
+++ src/futurevolto/config.js
@@ -1,8 +1,6 @@
-import ObjectWidget from './ObjectWidget';
 import ObjectBrowserWidget from '@plone/volto/components/manage/Widgets/ObjectBrowserWidget';
 
 const install = (config) => {
-  config.widgets.widget.object = ObjectWidget;
   config.widgets.widget.object_browser = ObjectBrowserWidget;
   return config;
 };
